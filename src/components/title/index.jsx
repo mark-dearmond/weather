4b6e1e7a0f184add6734ea56365f4e365fea556a
Mark DeArmond <mark.dearmond@kemper.com>
2021-09-29 09:14:37 -0500

    Added dynamic date

--- src/components/title/index.jsx
+++ src/components/title/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap';
+import Moment from 'react-moment';
 import './styles.css';
 
 export const Title = () => {
+    const today = new Date()
     return (
         <Row className="title text-center">
             <Col>
                 <h3>Dallas, TX</h3>
-                <p>Saturday, Sep 16, 2018</p>
+                <p>
+                    <Moment format={'dddd, MMM D, YYYY '}>{today}</Moment>
+                </p>
             </Col>
         </Row>
     )
