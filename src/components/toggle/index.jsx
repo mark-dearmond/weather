74f013f5d96640f6b62a2518c79ec6419ae3473d
Mark DeArmond <mark.dearmond@kemper.com>
2021-09-29 08:35:03 -0500

    Added toggle functionality

--- src/components/toggle/index.jsx
+++ src/components/toggle/index.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './styles.css';
 
 export const Toggle = () => {
-
+    const [unit, setUnit] = useState('imperial');
+    const handleUnitChange = () => {
+        if(unit === 'imperial') {
+            setUnit('metric')
+        } else{
+            setUnit('imperial')  
+        } 
+    }
     return (
         <label className="switch">
-            <input type="checkbox"/>
+            <input onClick={handleUnitChange} type="checkbox" checked={unit === 'imperial' ? true : false}/>
             <span className="slider round"></span>
-            <span className="temperature"></span>
+            <span className={`units ${unit}`}></span>
         </label>
     )
 }
