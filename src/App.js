f22e5ec74b73888715038359394d1f2ac721ecdf
Mark DeArmond <mark.dearmond@kemper.com>
2021-09-29 20:45:28 -0500

    Added spinning loader

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap";
 import { Title } from "./components/title";
 import { Frame } from "./components/frame";
 import { store } from './context/store';
+import Loader from "react-loader-spinner";
 
 const APP_ID = 'd3f745d37052d30ab8fb8bb5dea8eb55';
 const LATITUDE = '32.7767';
@@ -30,9 +31,8 @@ function App() {
 
   return (
       <Container>
-        <Title></Title>
-        {!state.isLoading && state.fahrenheit && <Frame></Frame>}
-        {state.isLoading && 'Loading...'}
+        {!state.isLoading && state.fahrenheit && <Title></Title> && <Frame></Frame>}
+        {state.isLoading && <Loader className="d-flex align-items-center justify-content-center" type="TailSpin" color="#FFFFFF" height={100} width={100}/>}
       </Container> 
   );
 }
